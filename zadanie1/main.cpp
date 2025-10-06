fb3b960c2b89ce5b1eed46250e36c6f1c320fdb8
mezin24pt1 <andrejmezin713@gmail.com>
2025-10-06 21:41:59 +0300

    Update main.cpp

---
+++ zadanie1/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <locale>
+#include <codecvt>
+#include "modAlphaCipher.h"
+using namespace std;
+std::wstring stringToWstring(const std::string& str);
+std::string wstringToString(const std::wstring& wstr);
+
+int main() {
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+
+    string key_input;
+    cout << "Шифр готов. Введите ключ: ";
+    getline(cin, key_input);
+    wstring key = stringToWstring(key_input);
+
+    try {
+        modAlphaCipher cipher(key); // ← может бросить исключение
+        cout << "Ключ загружен" << endl;
+
+        unsigned op;
+        do {
+            cout << "Шифр готов. Выберите операцию (0-выход, 1-шифрование, 2-расшифрование): ";
+            cin >> op;
+            cin.ignore();
+
+            if (op > 2) {
+                cout << "Неверная операция" << endl;
+            } else if (op > 0) {
+                string text_input;
+                cout << "Введите текст: ";
+                getline(cin, text_input);
+                wstring text = stringToWstring(text_input);
+
+                try {
+                    if (op == 1) {
+                        wstring encrypted = cipher.encrypt(text);
+                        cout << "Зашифрованный текст: " << wstringToString(encrypted) << endl;
+                    } else {
+                        wstring decrypted = cipher.decrypt(text);
+                        cout << "Расшифрованный текст: " << wstringToString(decrypted) << endl;
+                    }
+                } catch (const cipher_error& e) {
+                    cout << "Ошибка: " << e.what() << endl;
+                }
+            }
+        } while (op != 0);
+
+    } catch (const cipher_error& e) {
+        cerr << "Ошибка: " << e.what() << endl;
+        return 1;
+    }
+
+    return 0;
+}
+std::wstring stringToWstring(const std::string& str) {
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    return converter.from_bytes(str);
+}
+
+std::string wstringToString(const std::wstring& wstr) {
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    return converter.to_bytes(wstr);
+}
