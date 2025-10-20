efac4ac3dad08b595d4ccc5f44d4bc14a714f639
mezin24pt1 <andrejmezin713@gmail.com>
2025-10-20 17:36:23 +0300

    Update modAlphaCipher.h

--- zadanie1/modAlphaCipher.h
+++ zadanie1/modAlphaCipher.h
@@ -1,33 +1,30 @@
-#pragma once
+#ifndef MODALPHACIPHER_H
+#define MODALPHACIPHER_H
+
 #include <vector>
 #include <string>
 #include <map>
-#include <stdexcept> 
+#include <locale>
+#include <codecvt>
+#include <stdexcept>
 
-class cipher_error : public std::invalid_argument {
-public:
-    explicit cipher_error(const std::string& what_arg) : std::invalid_argument(what_arg) {}
-    explicit cipher_error(const char* what_arg) : std::invalid_argument(what_arg) {}
-};
+using namespace std;
 
 class modAlphaCipher
 {
 private:
-    std::wstring numAlpha = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-    std::map<wchar_t, int> alphaNum;
-    std::vector<int> key;
-
-
-    std::wstring getValidKey(const std::wstring& s);
-    std::wstring getValidOpenText(const std::wstring& s);
-    std::wstring getValidCipherText(const std::wstring& s);
-
-    std::vector<int> convert(const std::wstring& s);
-    std::wstring convert(const std::vector<int>& v);
+    const wstring numAlpha = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"; 
+    map<wchar_t, int> alphaNum; 
+    vector<int> key; 
+    
+    vector<int> convert(const wstring& text);
+    wstring convert(const vector<int>& numbers);
 
 public:
-    modAlphaCipher() = delete;
-    modAlphaCipher(const std::wstring& skey);
-    std::wstring encrypt(const std::wstring& open_text);
-    std::wstring decrypt(const std::wstring& cipher_text);
+    modAlphaCipher() = delete; 
+    modAlphaCipher(const wstring& skey);
+    wstring encrypt(const wstring& open_text);
+    wstring decrypt(const wstring& cipher_text);
 };
+
+#endif 
