769329a0061e8a7a28f1c75ed28515cb7b6c690a
mezin24pt1 <andrejmezin713@gmail.com>
2025-11-09 15:37:00 +0300

    Update table_cipher.h

--- zadanie2/table_cipher.h
+++ zadanie2/table_cipher.h
@@ -1,19 +1,29 @@
-#ifndef TABLE_CIPHER_H
-#define TABLE_CIPHER_H
-
+#pragma once
 #include <string>
+#include <vector>
 #include <stdexcept>
 using namespace std;
 
-class TableCipher
-{
-private:
-    int columns;
-    
+class cipher_error: public invalid_argument {
 public:
-    TableCipher(int key);
-    wstring encrypt(const wstring& plain_text);
-    wstring decrypt(const wstring& cipher_text);
+    explicit cipher_error (const string& what_arg):
+        invalid_argument(what_arg) {}
+    explicit cipher_error (const char* what_arg):
+        invalid_argument(what_arg) {}
 };
 
-#endif
+class Table
+
+{
+    
+private:
+    int cols;
+    int getValidKey(const int key);
+    wstring getValidOpenText(const wstring& s);
+    wstring getValidCipherText(const wstring& s);
+    
+public:
+    explicit Table(int key);
+    wstring encrypt(const wstring& plain);
+    wstring decrypt(const wstring& cipher);
+};
